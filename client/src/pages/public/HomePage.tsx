e82237a0c8eac168d921e6871694b3af78caa013
Dahir Muse Abdulle <138979678+duceysaneyare@users.noreply.github.com>
2026-02-01 15:05:50 +0300

    Update HomePage.tsx

--- client/src/pages/public/HomePage.tsx
+++ client/src/pages/public/HomePage.tsx
@@ -31,7 +31,7 @@ const HomePage = () => {
   return (
     <>
       <Helmet>
-        <title>Mr. Nour Haji Osman | Public Health Expert</title>
+        <title>Nour Haji Osman | Public Health Expert</title>
         <meta
           name="description"
           content="Building resilient health information systems across the Horn of Africa."
